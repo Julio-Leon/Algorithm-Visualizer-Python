ff757666ac21571bfd80da396bcdbd0fa05711cc
julileon10 <julio.leon.diaz1@gmail.com>
2021-12-07 13:11:51 -0500

    fixed spelling

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ pygame.init()
 
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption("Dijkstra Path Findind Algorithm with PyGame")
+pygame.display.set_caption("Dijkstra Path Finder Algorithm with PyGame")
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
